bd913dbad938617ab074bfbc4666ee6a70231007
Vincent <vincentreix54@gmail.com>
2023-12-08 09:28:29 +0100

    Initial Commit

---
+++ src/test/java/fr/polytech/devfullstack/DevFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.polytech.devfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevFullStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
